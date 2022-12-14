be4001cee66ca9bdf9c134b5bea20e195d89f577
Abdoul-Shadow <abdoulrachidboni@gmail.com>
2022-12-13 16:16:28 -0800

    finish

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,18 @@
-#include -stdio.h
+#include <stdio.h>
+
+
+
 /**
- * main - A program that print with printf
- * Return: 0 (success)
+ * main - Prints a text
+ * Return: Always 0 (Success)
  */
 
 int main(void)
+
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
 }
